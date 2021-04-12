8e0071114a9d37d22ca005a1fec9a0f5d7a1eb5f
f-person <agakaryan.arshak@gmail.com>
2021-04-12 12:55:12 +0400

    refactor: remove debug print

--- lua/lua-timeago/init.lua
+++ lua/lua-timeago/init.lua
@@ -29,7 +29,6 @@ local function round(num) return math.floor(num + 0.5) end
 ---@param time osdate
 ---@return string
 function M.format(time)
-    print('hello')
     local now = os.time()
     local diff_seconds = os.difftime(now, time)
     if diff_seconds < 45 then return language.justnow end
