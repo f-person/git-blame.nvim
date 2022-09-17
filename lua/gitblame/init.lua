5501a674c57c45e869bf9634ef744135965d0112
eriedaberrie <eriedaberrie@gmail.com>
2022-09-17 15:10:24 -0700

    feat: Ignore special buffers

--- lua/gitblame/init.lua
+++ lua/gitblame/init.lua
@@ -103,6 +103,9 @@ local function load_blames(callback)
     local filepath = vim.api.nvim_buf_get_name(0)
     if filepath == "" then return end
 
+    local buftype = vim.api.nvim_buf_get_option(0, 'bt')
+    if buftype ~= '' then return end
+
     local filetype = vim.api.nvim_buf_get_option(0, 'ft')
     if vim.tbl_contains(vim.g.gitblame_ignored_filetypes, filetype) then
         return
