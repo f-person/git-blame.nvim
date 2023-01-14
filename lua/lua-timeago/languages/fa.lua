9dc6038d0a3c4c5ee100d1c991f9e7815081e8ec
Amaan Q <amaanq12@gmail.com>
2023-01-14 15:04:23 -0500

    chore: bump lua-timeago

---
+++ lua/lua-timeago/languages/fa.lua
@@ -0,0 +1,9 @@
+return {
+    justnow = 'هم‌اکنون',
+    minute = {singular = 'یک دقیقه پیش', plural = 'دقایقی پیش'},
+    hour = {singular = 'یک ساعت پیش', plural = 'ساعاتی پیش'},
+    day = {singular = 'یک روز پیش', plural = 'روزها پیش'},
+    week = {singular = 'یک هفته پیش', plural = 'هفته‌ها پیش'},
+    month = {singular = 'یک ماه پیش', plural = 'ماه‌ها پیش'},
+    year = {singular = 'یک سال پیش', plural = 'سال‌ها پیش'}
+}
