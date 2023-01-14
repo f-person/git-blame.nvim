934e10726afe4dfbb44cbc903abd8bf15213d5f7
Amaan Q <amaanq12@gmail.com>
2023-01-14 15:04:23 -0500

    chore: bump lua-timeago

---
+++ lua/lua-timeago/languages/fr.lua
@@ -0,0 +1,9 @@
+return {
+    justnow = 'maintenant',
+    minute = {singular = "il y'a une minute",  plural = "il y'a une minute"},
+    hour   = {singular = "il y'a une heure",   plural = "il y'a une heure"},
+    day    = {singular = "il y'a une journée", plural = "il y'a une journée"},
+    week   = {singular = "il y'a une semaine", plural = "il y'a une semaine"},
+    month  = {singular = "il y'a une mois",    plural = "il y'a une mois"},
+    year   = {singular = "il y'a une année",   plural = "il y'a une année"}
+}
