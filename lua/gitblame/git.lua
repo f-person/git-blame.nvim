71e16fc9e2d746ed9f5c32c25cdb1389a0f29c24
Sam Bossley <sam@bossley.xyz>
2024-02-27 15:51:36 -0500

    chore: add fallback comment

--- lua/gitblame/git.lua
+++ lua/gitblame/git.lua
@@ -118,6 +118,8 @@ end
 ---@param callback fun(url: string)
 function M.get_file_url(filepath, sha, line1, line2, callback)
     M.get_repo_root(function(root)
+        -- if outside a repository, return the filepath
+        -- so we can still copy the path or open the file
         if root == "" then
             callback(filepath)
             return
