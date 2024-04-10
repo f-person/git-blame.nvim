121866371fca42c3a870de166139d40d00e0768b
David Gao <gao.ten@northeastern.edu>
2024-04-09 23:56:23 -0400

    docs: Add Chinese language on changing lua-timeago languages

---
+++ lua/lua-timeago/languages/zh.lua
@@ -0,0 +1,9 @@
+return {
+    justnow = "刚刚",
+    minute = { singular = "1分钟前", plural = "分钟前" },
+    hour = { singular = "1小时前", plural = "小时前" },
+    day = { singular = "1天前", plural = "天前" },
+    week = { singular = "1周前", plural = "周前" },
+    month = { singular = "1月前", plural = "月前" },
+    year = { singular = "1年前", plural = "年前" },
+}
